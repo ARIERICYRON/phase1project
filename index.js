085212ae788e80c43dfa83170a9a5e3bdd8fc73c
arieri <ariericyron@gmail.com>
2022-06-27 08:14:34 +0300

    added a READ.me file

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const init = () => {
   document.addEventListener('DOMContentLoaded', init);
 }
 
+
 const api_url = ('https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json')
 async function getVehicle(Make_ID, Name_ID){
    const response = await fetch (api_url)
    const data = await response.json()
    console.log(data)
-   document.getElementById('container').innerHTML = data.Make_ID
-   document.getElementById('container').innerHTML = data.Name_ID
+   document.getElementById('ID').innerHTML = data.Make_ID
+   document.getElementById('NAME').innerHTML = data.Name_ID
   
 }
 getVehicle()
